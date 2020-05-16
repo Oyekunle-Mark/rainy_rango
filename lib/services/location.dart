ff28918a906def05ce3a09ad99f8e3881d816605
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-16 08:38:14 +0100

    Refactored the getLocation method

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,10 +1,16 @@
 import 'package:geolocator/geolocator.dart';
 
 class Location {
-  Future<Position> getCurrentLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+  double _latitude;
+  double _longitude;
 
-    return position;
+  void getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+      _latitude = position.latitude;
+      _longitude = position.longitude;
+    } catch (e) {}
   }
 }
